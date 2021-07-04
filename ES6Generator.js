e3d9f19f2e02e9cc52d71f838590ba6e3f612ca4
readwanmd <readwanmd@gmail.com>
2021-07-04 11:26:09 +0600

    Day - 9

--- ES6Generator.js
+++ ES6Generator.js
@@ -89,3 +89,91 @@ for (let element of iterator) {
 	console.log(element);
 }
 // console.log(...iterator);
+
+//Example: 2
+function* range(start, end, step) {
+	let current = start;
+	while (current <= end) {
+		yield current;
+		current += step;
+	}
+}
+
+let iterator1 = range(1, 1000, 2);
+console.log(iterator1.next());
+console.log(iterator1.next());
+console.log(iterator1.next());
+
+// Control Flow of generator
+function* generator1(a, b) {
+	let k = yield a + b;
+	let m = yield a + b + k;
+
+	yield a + b + k + m;
+}
+
+let genn = generator1(10, 20);
+console.log(genn.next());
+console.log(genn.next(50));
+console.log(genn.next(100));
+
+//async/await & generator
+
+const takeOrder = (customer) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(`Order taken from ${customer}`);
+		}, 1000);
+	});
+};
+
+const processOrder = (customer) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(`Order processed for ${customer}`);
+		}, 2000);
+	});
+};
+
+const completeOrder = (customer) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(`Order complete for ${customer}`);
+		}, 1000);
+	});
+};
+
+async function* solution(customer) {
+	yield await takeOrder(customer);
+	yield await processOrder(customer);
+	yield await completeOrder(customer);
+}
+
+let sltn = solution('Readwan');
+
+// sltn.next().then(({ value }) => console.log(value));
+// sltn.next().then(({ value }) => console.log(value));
+// sltn.next().then(({ value }) => console.log(value));
+
+const promises = [sltn.next(), sltn.next(), sltn.next()];
+
+(async function () {
+	for await (p of promises) {
+		console.log(p);
+	}
+})();
+
+//async iterator
+const myAsyncIterable = {
+	async *[Symbol.asyncIterator]() {
+		yield 'hello';
+		yield 'async';
+		yield 'iterator';
+	},
+};
+
+(async () => {
+	for await (x of myAsyncIterable) {
+		console.log(x);
+	}
+})();
